3ec03a5cccf75d89825b8ce94077f3965e1e03cf
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2021-12-24 23:32:25 +0200

    Style improvements
    - function and argument naming
    - `new` constructor for `Match`
    - doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -13,21 +13,44 @@ pub struct Match {
     live_index: usize,
 }
 
-fn find_next_match_after(score: &[ScoreNote], score_index: usize, pitch: u8) -> Option<usize> {
-    score[score_index..]
-        .iter()
-        .position(|score_note| score_note.pitch == pitch)
-        .map(|i| score_index + i)
+impl Match {
+    pub fn new(score_index: usize, live_index: usize) -> Self {
+        Self {
+            score_index,
+            live_index,
+        }
+    }
 }
 
-fn time_difference(
-    score: &[ScoreNote],
-    index1: usize,
-    index2: usize,
-) -> u32 {
+/// Finds the next note with given `pitch`, starting from `score[index]`
+fn find_next_match_starting_at(score: &[ScoreNote], index: usize, pitch: u8) -> Option<usize> {
+    score[index..]
+        .iter()
+        .position(|note| note.pitch == pitch)
+        .map(|i| index + i)
+}
+
+/// Calculates the time difference between notes `score[index1]` and `score[index2]`
+fn time_difference(score: &[ScoreNote], index1: usize, index2: usize) -> u32 {
     score[index2].time - score[index1].time
 }
 
+/// Finds matches in the score for new notes in the live performance
+///
+/// # Arguments
+///
+/// * score - The complete expected musical score with timestamps and pitches
+/// * live - The live performance recorded so far, with timestamps and pitches
+/// * prev_match_score_index - Index of the last note so far which has been matched
+///                            between the live performance and the expected score
+/// * new_live_index - Index of the first new note received for the live performance
+///                    since the previous round
+///
+/// # Return value
+///
+/// A 2-tuple of
+/// * newly found matches between the live performance and the expected score
+/// * ignored new input notes (as a list of live performance indices)
 fn find_new_matches(
     score: &[ScoreNote],
     live: &[ScoreNote],
@@ -35,19 +58,16 @@ fn find_new_matches(
     new_live_index: usize,
 ) -> (Vec<Match>, Vec<usize>) {
     let mut score_pointer = match prev_match_score_index {
+        Some(i) => i + 1, // continue in the score just after last previous match, or
         None => 0,        // start from beginning of score if nothing matched yet
-        Some(i) => i + 1, // continue in the score just after last previous match
     };
     let mut matches: Vec<Match> = vec![];
     let mut ignored: Vec<usize> = vec![];
     for (live_index, live_note) in live.iter().enumerate().skip(new_live_index) {
-        let matching_index = find_next_match_after(score, score_pointer, live_note.pitch);
+        let matching_index = find_next_match_starting_at(score, score_pointer, live_note.pitch);
         match matching_index {
             Some(score_index) => {
-                matches.push(Match {
-                    score_index,
-                    live_index,
-                });
+                matches.push(Match::new(score_index, live_index));
                 score_pointer = score_index + 1;
             }
             None => ignored.push(live_index),
@@ -56,18 +76,39 @@ fn find_new_matches(
     (matches, ignored)
 }
 
-fn get_stretch_factor(
-    elapsed_score: u32,
-    elapsed_live: u32,
-) -> f32 {
+/// Calculates the stretch factor from elapsed time in the score and the live performance
+///
+/// # Arguments
+///
+/// * elapsed_score - Time elapsed between two notes in the expected score
+/// * elapsed_live - Time elapsed between the same notes in the live performance
+///
+/// # Return value
+///
+/// The ratio between `elapsed_live` and `elapsed_score`
+fn get_stretch_factor(elapsed_score: u32, elapsed_live: u32) -> f32 {
     (elapsed_live as f32) / (elapsed_score as f32)
 }
 
 /// Returns the score time in milliseconds corresponding to the latest live note
 /// (whether matched or unmatched)
 ///
-/// Corrects the elapsed time since the last match using the calculated stretch factor,
-/// and adds that to the score time of the last match.
+/// This can be the exact time index of the latest performed note in the expected score,
+/// in case a match was found for that performed note. If no match was found (i.e. it's
+/// a wrong or an extra note), an estimate based on the last previous match and the
+/// current time stretch factor is returned.
+///
+/// # Arguments
+///
+/// * score - The complete expected musical score with timestamps and pitches
+/// * live - The live performance recorded so far, with timestamps and pitches
+/// * prev_match - The index, in the score and in the live performance, for a matching
+///                note.
+/// * stretch_factor - The time stretch factor to use
+///
+/// # Return value
+///
+/// The estimated current time in the expected score in milliseconds.
 fn get_score_time(
     score: &[ScoreNote],
     live: &[ScoreNote],
@@ -91,6 +132,7 @@ fn get_score_time(
 ///
 /// # Example
 ///
+/// ```text
 /// |                        111
 /// |        time: 0123456789012
 /// |                  v------------ prev_match_score_index == 1 (last matched note)
@@ -101,6 +143,7 @@ fn get_score_time(
 /// |                  ^   ^   ^----
 /// |                  |   `-------- new_live_index == 2 (first newly received note)
 /// |                  `------------ prev_match_live_index == 1 (last matched node)
+/// ```
 ///
 /// This would return
 /// * 8 as the timestamp of the score since that's when the last live note (F) occurs in
@@ -125,11 +168,10 @@ fn get_score_time(
 ///
 /// # Return value
 ///
-/// A 3-tuple of
+/// A 4-tuple of
 /// * the timestamp of the score at the last new input note
 /// * the time stretch factor at the last new matching input note
-/// * the index of the last matched note in the score
-/// * the index of the last matched note in the live performance
+/// * for all matched notes, the index in the score and in the live performance
 /// * ignored new input notes as a list of live performance indices
 pub fn follow_score(
     score: &[ScoreNote],
@@ -154,10 +196,8 @@ pub fn follow_score(
     let last_two = matches.rev().take(2).collect::<Vec<&Match>>();
     let stretch_factor = match last_two[..] {
         [last, second_last] => {
-            let elapsed_score =
-                time_difference(score, second_last.score_index, last.score_index);
-            let elapsed_live =
-                time_difference(live, second_last.live_index, last.live_index);
+            let elapsed_score = time_difference(score, second_last.score_index, last.score_index);
+            let elapsed_live = time_difference(live, second_last.live_index, last.live_index);
             get_stretch_factor(elapsed_score, elapsed_live)
         }
         _ => prev_stretch_factor,
@@ -189,13 +229,7 @@ mod tests {
             follow_score(&score, &live, None, 0, 1.0);
         assert_eq!(time, 1000);
         assert_approx_eq!(stretch_factor, 1.0);
-        assert_eq!(
-            new_matches,
-            [Match {
-                score_index: 0,
-                live_index: 0
-            }]
-        );
+        assert_eq!(new_matches, [Match::new(0, 0)]);
         assert!(ignored.is_empty());
     }
 
@@ -206,104 +240,48 @@ mod tests {
             follow_score(&TEST_SCORE, &live, None, 0, 1.0);
         assert_eq!(time, 1000);
         assert_approx_eq!(stretch_factor, 1.0);
-        assert_eq!(
-            new_matches,
-            [Match {
-                score_index: 0,
-                live_index: 0
-            }]
-        );
+        assert_eq!(new_matches, [Match::new(0, 0)]);
         assert!(ignored.is_empty());
     }
 
     #[test]
     fn match_second() {
         let live = notes![(5, 60), (55, 62)];
-        let (time, stretch_factor, new_matches, ignored) = follow_score(
-            &TEST_SCORE,
-            &live,
-            Some(Match {
-                score_index: 0,
-                live_index: 0,
-            }),
-            1,
-            1.0,
-        );
+        let (time, stretch_factor, new_matches, ignored) =
+            follow_score(&TEST_SCORE, &live, Some(Match::new(0, 0)), 1, 1.0);
         assert_eq!(time, 1100);
         assert_approx_eq!(stretch_factor, 0.5);
-        assert_eq!(
-            new_matches,
-            [Match {
-                score_index: 1,
-                live_index: 1,
-            }]
-        );
+        assert_eq!(new_matches, [Match::new(1, 1)]);
         assert!(ignored.is_empty());
     }
 
     #[test]
     fn skip_extra_note() {
         let live = notes![(5, 60), (25, 61), (55, 62)];
-        let (time, stretch_factor, new_matches, ignored) = follow_score(
-            &TEST_SCORE,
-            &live,
-            Some(Match {
-                score_index: 0,
-                live_index: 0,
-            }),
-            1,
-            1.0,
-        );
+        let (time, stretch_factor, new_matches, ignored) =
+            follow_score(&TEST_SCORE, &live, Some(Match::new(0, 0)), 1, 1.0);
         assert_eq!(time, 1100);
         assert_approx_eq!(stretch_factor, 0.5);
-        assert_eq!(
-            new_matches,
-            [Match {
-                score_index: 1,
-                live_index: 2,
-            }]
-        );
+        assert_eq!(new_matches, [Match::new(1, 2)]);
         assert_eq!(ignored, vec![1]);
     }
 
     #[test]
     fn skip_missing_note() {
         let live = notes![(5, 60), (55, 64)];
-        let (time, stretch_factor, new_matches, ignored) = follow_score(
-            &TEST_SCORE,
-            &live,
-            Some(Match {
-                score_index: 0,
-                live_index: 0,
-            }),
-            1,
-            1.0,
-        );
+        let (time, stretch_factor, new_matches, ignored) =
+            follow_score(&TEST_SCORE, &live, Some(Match::new(0, 0)), 1, 1.0);
         assert_eq!(time, 1200);
         assert_approx_eq!(stretch_factor, 0.25);
-        assert_eq!(
-            new_matches,
-            [Match {
-                score_index: 2,
-                live_index: 1,
-            }]
-        );
+        assert_eq!(new_matches, [Match::new(2, 1)]);
         assert!(ignored.is_empty());
     }
 
     #[test]
     fn only_wrong_notes() {
         let live = notes![(5, 60), (55, 63), (105, 66)];
-        let (time, stretch_factor, new_matches, ignored) = follow_score(
-            &TEST_SCORE,
-            &live,
-            Some(Match {
-                score_index: 0,
-                live_index: 0,
-            }),
-            1,
-            1.0,
-        );
+        let (time, stretch_factor, new_matches, ignored) =
+            follow_score(&TEST_SCORE, &live, Some(Match::new(0, 0)), 1, 1.0);
         assert_eq!(time, 1100);
         assert_approx_eq!(stretch_factor, 1.0);
         assert!(new_matches.is_empty());
